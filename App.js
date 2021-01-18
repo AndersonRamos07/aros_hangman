905f11aa6f3fc75e17b264e96926e10426191059
Anderson Ramos <andersonramos07@live.com>
2021-01-18 18:03:40 -0300

    Iniciando os estudos...

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import styles from './Style';
 
 export default function App() {
   return (
@@ -11,12 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

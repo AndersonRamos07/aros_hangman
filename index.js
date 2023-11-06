8f061d6616d81a0110d10b7d723424ba2ebe5b09
Anderson_Ramos_07 <andersonramos07@live.com>
2023-11-05 19:15:18 -0500

    adjusting...

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
 const express = require('express');
-
+const path = require('node:path');
+const router = express.Router();
 const app = express();
 
-app.use('/', (req, res)=>
-{
-    res.send('Hello world!')
-});
+app.set('view engine', 'ejs');
 
-app.listen(8000, () => console.log('running in port: 8000...'))
+const gameRouter = require('./routers/games');
+app.use('/', gameRouter);
+
+app.use((req, res) =>
+{    res.status(404).render('notfound');    })
+
+
+app.listen(process.env.PORT, () =>{
+    console.log(`running in port: ${process.env.PORT}...`)
+});
+    
+    // const jotaesse = path.join(__dirname, 'public');
+    // console.log(jotaesse)
+    // app.use(express.static(jotaesse));
